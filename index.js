29450b8d0ba3b89c80657532840fbc01f03fc586
Maor Leger <maorleger@microsoft.com>
2024-11-15 13:05:49 -0800

    demo

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const lro2 = require("@azure/core-lro2");
 const lro3 = require("@azure/core-lro3");
 
+/**
+ * In core-lro2, the poller can be created synchronously or asynchronously and returned correctly.
+ */
 const getLro2 = async () => {
   return await lro2.createHttpPoller({
     sendInitialRequest: () => {
@@ -33,10 +36,9 @@ const getLro2 = async () => {
   });
 };
 
-const getLro3Async = async () => {
-  return getLro3Sync();
-};
-
+/**
+ * In core-lro3, the poller can be created synchronously and returned correctly.
+ */
 const getLro3Sync = () => {
   return lro3.createHttpPoller({
     sendInitialRequest: () => {
@@ -75,6 +77,19 @@ const getLro3Sync = () => {
     },
   });
 };
+
+/**
+ * Imagine I have an async function that returns a poller. For example, in core-lro v2 we may have done something like:
+ * - Create the poller
+ * - await poller.poll()
+ * - return the poller
+ *
+ * Because promises are unwrapped recursively in JavaScript, the poller is returned as a resolved promise of the operation. It's the equivalent of returning the awaited result of the polling operation. There's no way to "shallow" unwrap the promise and return the poller itself.
+ */
+const getLro3Async = async () => {
+  return getLro3Sync();
+};
+
 async function main() {
   const lr2 = await getLro2();
   const lr3 = getLro3Sync();
